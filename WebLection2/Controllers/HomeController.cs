cb7384e7fe2740211b4c054d7581053069ba5ee2
vladyslav-keidaliuk <102413334+vladyslav-keidaliuk@users.noreply.github.com>
2023-04-16 20:39:46 +0300

    Add project files.

---
+++ WebLection2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebLection2.Models;
+
+namespace WebLection2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Entry() 
+        {
+            return View("EntryView"); 
+        }
+       
+
+        [HttpGet]
+        public ViewResult Input()
+        {
+            return View("InputView");
+        }
+
+        [HttpPost]
+        public ViewResult Input(PersonReview guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                guestResponse.CreatedAt = DateTime.Now.ToString("dd-MMM-yy", new System.Globalization.CultureInfo("en-US"));
+                Repository.AddResponse(guestResponse);
+                return View("ThanksView", guestResponse);
+            }
+            else
+            {
+                return View("InputView");
+            }
+        }
+
+        public ViewResult List()
+        {
+            return View("ListView", Repository.Responses);
+                
+        }
+    }
+
+}
